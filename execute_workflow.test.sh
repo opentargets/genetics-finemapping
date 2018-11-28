b32548353de4a791db33c7e18fa346531fcbc031
edm1 <emountjoy@gmail.com>
2018-11-28 15:00:32 +0000

    PATH to cromwell jar

--- execute_workflow.test.sh
+++ execute_workflow.test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 # Run cromwell
 java -Dconfig.file=configs/cromwell.config \
-     -jar ~/software/cromwell_36/cromwell-36.jar run workflows/finemapping.wdl \
+     -jar $CROMWELL_JAR run workflows/finemapping.wdl \
      --inputs configs/workflow.config.json
 
 echo COMPLETE
