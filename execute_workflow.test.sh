bedfbd85d6d43a0f30aac8062030f26b97f8db1b
edm1 <emountjoy@gmail.com>
2018-11-29 16:55:42 +0000

    write cromwell output to log

--- execute_workflow.test.sh
+++ execute_workflow.test.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 mkdir -p logs
 java -Dconfig.file=configs/cromwell.config \
      -jar $CROMWELL_JAR run workflows/finemapping.wdl \
-     --inputs configs/workflow.config.json \
-     > logs/workflow_log.txt
+     --inputs configs/workflow.config.json
 
 echo COMPLETE
