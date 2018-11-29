f7855f9402d357cabdd7cd9cb6a87abcd430b15f
edm1 <emountjoy@gmail.com>
2018-11-29 14:26:54 +0000

    install docker

--- setup.sh
+++ setup.sh
@@ -35,22 +35,22 @@ cd $HOME
 sudo su -
 
 # Install docker: https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
-# apt-get remove docker docker-engine docker.io
-# apt-get update
-# apt-get install \
-#     apt-transport-https \
-#     ca-certificates \
-#     curl \
-#     software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-# apt-key fingerprint 0EBFCD88
-# add-apt-repository \
-#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#    $(lsb_release -cs) \
-#    stable"
-# apt-get update
-# apt-get install docker-ce
-# docker run hello-world # Test install
+apt-get remove docker docker-engine docker.io
+apt-get update
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install docker-ce
+docker run hello-world # Test install
 
 # Install JRE
 apt install -yf openjdk-8-jre-headless
