93489a5ed52afede3010c3effc66d799cdceba2b
edm1 <emountjoy@gmail.com>
2019-03-14 17:04:51 +0000

    test run config

--- 4_run_commands.sh
+++ 4_run_commands.sh
@@ -3,12 +3,12 @@
 
 set -euo pipefail
 
-cores=4
-instance_name="em-finemap-local-ssd"
+cores=32
+instance_name="em-finemap-ssd"
 instance_zone="europe-west1-d"
 
 python 3_make_commands.py | parallel -j $cores
 
 echo COMPLETE
 
-# gcloud compute instances stop $instance_name --zone=$instance_zone
+gcloud compute instances stop $instance_name --zone=$instance_zone
