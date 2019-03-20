675b6fbff623ffdcc5e07db0734cd64d463c767b
edm1 <emountjoy@gmail.com>
2019-03-20 10:18:00 +0000

    copy results

--- 6_copy_results_to_gcs.sh
+++ 6_copy_results_to_gcs.sh
@@ -4,7 +4,7 @@
 version_date=`date +%y%m%d`
 
 # Copy results
-gsutil -m rsync -rn results gs://genetics-portal-staging/finemapping/$version_date
+gsutil -m rsync -r results gs://genetics-portal-staging/finemapping/$version_date
 
 # Tar the logs and copy over
 tar -zcvf logs.tar.gz logs
